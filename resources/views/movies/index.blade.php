cfd1034acd6ac7725b5faa65043c3c9ba421aba6
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-12-20 15:35:46 +0100

    addes styles to movies index and show

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,9 +2,16 @@
 @section('title' . 'Movies')
 
 @section('content')
-    <ul>
-        @foreach ($movies as $movie)
-            <li><a href="{{ route('movies.show', $movie->id) }}">{{ $movie->title }}</a></li>
-        @endforeach
-    </ul>
+    <div class="container mt-5">
+        <div class="row g-3 ">
+            @foreach ($movies as $movie)
+                <div class="col-12 col-mg-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="{{ $movie->image }}" alt="{{ $movie->title }}">
+                        <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-primary">Info</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -3,5 +3,11 @@
 @section('title', '$movie->title')
 
 @section('content')
-    <h1>{{ $movie->title }}</h1>
+    <div class="container d-flex justify-content-between mt-5">
+        <div>
+            <h1>{{ $movie->title }}</h1>
+            <h2 class="mt-5">Titolo Originale: <span class="bg-white p-2 rounded">{{ $movie->original_title }}</span></h2>
+        </div>
+        <img src="{{ $movie->image }}" alt="{{ $movie->title }}" style="width: 18rem">
+    </div>
 @endsection
